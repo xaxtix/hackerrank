3b1d994daf3b8bdf679adb55b0c0a917685d1dab
Илья Самородов <ilsamorodov@gmail.com>
2017-12-17 01:58:14 +0300

    initial commit

---
+++ src/com/isamorodov/utils/DecimalUtils.java
@@ -0,0 +1,20 @@
+package com.isamorodov.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xaxtix on 16.12.2017.
+ */
+public class DecimalUtils {
+
+    /**
+     * Раскладывает
+     * @param d
+     * @return список делителей
+     */
+    public List<Integer> getAllDividers(int d){
+        List<Integer> list = new ArrayList<>();
+        return list;
+    }
+}
